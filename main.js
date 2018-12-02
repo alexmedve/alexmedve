e01ade214e7507e24cf399f939efe49028d16ca9
alexmedve <medve_a@yahoo.com>
2018-12-02 21:45:08 +0200

    added image gallery

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
         $("nav ul").toggleClass("showing");
     });
 });
-
 //scroll efect
-
 $(window).on("scroll", function() {
     if($(window).scrollTop()) {
         $("nav").addClass('black');
@@ -14,3 +12,26 @@ $(window).on("scroll", function() {
         $("nav").removeClass('black');
     }
 });
+
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+
+//set first image opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+    //reset the opacity
+    imgs.forEach(img => img.style.opacity = 1)
+    //change current image to src of clicked image
+    current.src = e.target.src;
+    //add fade in class
+    current.classList.add('fade-in');
+    //remove fade in class 
+    setTimeout(() => current.classList.remove('fade-in'), 500);
+    //change the opacity to opacity var
+    e.target.style.opacity = opacity;
+}
+
