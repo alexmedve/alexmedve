f325d7b7509c70238b7d3ab7efaf961c8cb06a5c
alex.medve <medvealex0@gmail.com>
2019-08-30 17:00:29 +0300

    added js animations

--- nicole/js/main.js
+++ nicole/js/main.js
@@ -1,11 +1,18 @@
 function init() {
-    new hoverEffect({
-        parent: document.querySelector('section.work'),
-        intensity: 0.2,
-        image1: '../assets/model.jpg',
-        image2: '../assets/runway.jpg',
-        displacementImage: '../assets/stripe1.png'
-    })
+    const hero = document.querySelector('.hero .inner .heading h1');
+    const about = document.querySelector('.hero .inner .heading a h3');
+    const work = document.querySelector('.work .inner .heading h2');
+    const personal = document.querySelector('.personal .inner .heading h2');
+    setTimeout(() => {
+        hero.classList.add('heading-anim');
+        about.classList.add('heading-anim');
+    }, 600);
+    setTimeout(() => {
+        work.classList.add('heading-anim');
+    }, 900);
+    setTimeout(() => {
+        personal.classList.add('heading-anim');
+    }, 1200);
 }
 
-init();
+window.onload = init;
