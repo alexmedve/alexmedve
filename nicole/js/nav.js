7907c351b766177c8fb9b66dd018785c964e3ac9
alex.medve <medvealex0@gmail.com>
2019-09-02 17:07:02 +0300

    finished navbar

--- nicole/js/nav.js
+++ nicole/js/nav.js
@@ -1,7 +1,5 @@
 function nav() {
 
-    //menu
-
     const hamburger = document.querySelector(".menu");
     // const hamburgerLines = document.querySelectorAll(".menu line");
     const navOpen = document.querySelector(".nav-open");
@@ -10,53 +8,57 @@ function nav() {
     const about = document.querySelector(".about-link");
     // const logo = document.querySelector(".logo");
 
-    const tl = new TimelineMax({
-        paused: true,
-        reversed: true
-    });
+    let opened = false;
 
-    tl.to(navOpen, 0.5, {
-            y: 0
-        })
-        .fromTo(work, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, "-=0.1")
-        .fromTo(personal, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, "-=0.5")
-        .fromTo(about, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, '-=0.5');
-    // .fromTo(logo, 0.2, {
-    //     color: "white"
-    // }, {
-    //     color: "black"
-    // }, "-=1")
-    // .fromTo(
-    //     hamburgerLines,
-    //     0.2, {
-    //         stroke: "white"
-    //     }, {
-    //         stroke: "black"
-    //     },
-    //     "-=1"
-    // );
+    function openMenu() {
+        //remove close animation classes
+        navOpen.classList.remove('nav-open-anim-reverse');
+        work.classList.remove('nav-link-anim-reverse');
+        about.classList.remove('nav-link-anim-reverse');
+        personal.classList.remove('nav-link-anim-reverse');
+        //add open animation classes
+        navOpen.classList.add('nav-open-anim');
+        work.classList.add('nav-link-anim');
+        personal.classList.add('nav-link-anim');
+        about.classList.add('nav-link-anim');
 
-    hamburger.addEventListener("click", () => {
-        tl.reversed() ? tl.play() : tl.reverse();
-    });
+        opened = true;
+    }
+
+    function closeMenu() {
+        //remove open animation classes
+        navOpen.classList.remove('nav-open-anim');
+        work.classList.remove('nav-link-anim');
+        about.classList.remove('nav-link-anim');
+        personal.classList.remove('nav-link-anim');
+        //add close animation classes
+        navOpen.classList.add('nav-open-anim-reverse');
+        work.classList.add('nav-link-anim-reverse');
+        personal.classList.add('nav-link-anim-reverse');
+        about.classList.add('nav-link-anim-reverse');
+        opened = false;
+    }
+
+    const throttle = (func, limit) => {
+        let inThrottle
+        return function () {
+            const args = arguments
+            const context = this
+            if (!inThrottle) {
+                func.apply(context, args)
+                inThrottle = true
+                setTimeout(() => inThrottle = false, limit)
+            }
+        }
+    }
+
+    hamburger.addEventListener('click', throttle(() => {
+        if (opened === false) {
+            openMenu();
+        } else {
+            closeMenu();
+        }
+    }, 700));
 }
 
 window.onload = nav;
